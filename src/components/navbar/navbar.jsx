a13367280e457253571e82994cfeec1d95d98708
Lucy <l.apitsaryan@innoverse.tech>
2021-08-23 12:15:07 +0400

    images added

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import BearImg from "../../images/bearImg.png"
+import "./navbar.scss"
+
+import {ReactComponent as BearFace} from "../../images/bear-face.svg"
+function Navbar() {
+    return (
+        <div>
+            <div className="nav">
+                <img src={BearImg} />
+                <BearFace/>
+            </div>
+        </div>
+    )
+}
+export default Navbar
