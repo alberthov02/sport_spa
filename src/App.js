55485645f575feb6dd8e5b14b030968734a7494f
Lucy <l.apitsaryan@innoverse.tech>
2021-08-23 15:40:11 +0400

    navbar is done, main page Bear

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import Navbar from './components/navbar/navbar';
+import Navbar from './components/Navbar/Navbar';
 import './fonts.css';
-
+import {ReactComponent as BearFace} from "./images/bear-face.svg"
+import { Route, Switch } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Navbar />
-      <p>
-        dddddddddddddddddddddddddd
-      </p>
+      <Switch>
+        <Route path="/" exact />
+        <Route path="/info" exact />
+        <Route path="/contact" exact />
+      </Switch>
+      <div className="first-block">
+      <div className="main-block">
+        <BearFace className="bear-face"/>
+        <button>Let me In</button>
+      </div>
+      </div>
+      
     </div>
   );
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,40 @@
-import React from "react"
-import BearImg from "../../images/bearImg.png"
-import "./navbar.scss"
-import {ReactComponent as BearFace} from "../../images/bear-face.svg"
-function Navbar() {
+import React, {useState} from "react"
+import {ReactComponent as Logo} from "../../images/logo.svg"
+import {ReactComponent as Filter} from "../../images/filter-icon.svg"
+import "../Navbar/Navbar.scss"
+import {NavLink} from "react-router-dom"
+function Navbar(props) {
+    const [isAuth, setIsAuth] = useState(false)
+    const [isFilterShow, setIsFilterShow] = useState(false);
+
+    const handleClick = () => {
+        setIsFilterShow(!isFilterShow)
+    }
     return (
         <div>
             <div className="nav">
-                <img src={BearImg} />
-                <BearFace/>
+                <div className="nav-logo-part">
+                    <Logo/>
+                    <div >
+                        <ul className="nav-menu">
+                            <li><NavLink exact to="/">Jogs</NavLink></li>
+                            <li><NavLink to="/info">Info</NavLink></li>
+                            <li><NavLink to="/contact">Contact US </NavLink></li>
+                            <button onClick={handleClick}><Filter className={isFilterShow?"filter-btn":"def-filter"}/></button>
+                        </ul>
+                    </div>
+                    
+                </div>
             </div>
+            {
+                isFilterShow && <div className="date-part">
+                    <label>Date from</label>
+                    <input type="date" id="aa" />
+                    <label>Date to</label>
+                    <input/>
+                 </div>
+            }
+            
         </div>
     )
 }
